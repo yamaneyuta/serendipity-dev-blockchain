35a01fbf22cbb587811dc5c1f86d1921c8dd33dd
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-11-09 03:06:34 +0000

    change: Appデプロイのコードを修正

--- src/features/contract/deployApp.ts
+++ src/features/contract/deployApp.ts
@@ -1,39 +1,19 @@
-import { ethers, Contract } from "ethers";
+import { ethers } from "ethers";
 
-import AppV1Data from "../../../artifacts/contracts/app/AppV1.sol/AppV1.json";
+import AppBytecode from "../../../artifacts/contracts/app/AppV1.sol/AppV1.bytecode.json";
+import AppAbi from "../../../artifacts/contracts/app/AppV1.sol/AppV1.abi.json";
+import { AppV1 } from "../../../typechain-types/contracts/app/AppV1";
 
 export const deployAppV1 = async (signer: ethers.Signer) => {
     // デプロイ実行
-    const factory = createAppV1Factory().connect(signer);
+    const factory = new ethers.ContractFactory<[], AppV1>(AppAbi.abi, AppBytecode.bytecode).connect(signer);
     const contract = await factory.deploy();
     await contract.waitForDeployment();
 
     // 初期化処理
     // ※本番環境ではdeployProxy等を使用し、このような初期化は絶対に行わないこと。
-    const tx = await (contract as unknown as Contract).initialize(await signer.getAddress());
+    const tx = await contract.initialize(await signer.getAddress());
     await tx.wait();
 
     return contract;
-};
-
-const createAppV1Factory = () => {
-    const abi = [
-        {
-            "inputs": [
-              {
-                "internalType": "address",
-                "name": "initialAdmin",
-                "type": "address"
-              }
-            ],
-            "name": "initialize",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-        }
-    ];
-
-    const bytecode = AppV1Data.bytecode;
-
-    return new ethers.ContractFactory<[], Contract>(abi, bytecode);
 };
