7d10bdc5210a4a6f376c19b228ae0995034d9226
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-11-09 03:07:58 +0000

    change: TrialOracleデプロイのコードを修正

--- src/features/contract/deployTrialOracle.ts
+++ src/features/contract/deployTrialOracle.ts
@@ -1,48 +1,14 @@
 import { ethers } from "ethers";
-import { ITrialOracleV1 } from "../../../typechain-types/contracts/privatenet/oracle/ITrialOracleV1";
 
-import ITrialOracleV1Data from "../../../artifacts/contracts/privatenet/oracle/ITrialOracleV1.sol/ITrialOracleV1.json";
-import TrialOracleV1Data from "../../../artifacts/contracts/privatenet/oracle/TrialOracleV1.sol/TrialOracleV1.json";
+import TrialOracleAbi from "../../../artifacts/contracts/privatenet/oracle/TrialOracleV1.sol/TrialOracleV1.abi.json";
+import TrialOracleBytecode from "../../../artifacts/contracts/privatenet/oracle/TrialOracleV1.sol/TrialOracleV1.bytecode.json";
+import { TrialOracleV1 } from "../../../typechain-types/contracts/privatenet/oracle/TrialOracleV1";
 
 export const deployTrialOracleV1 = async (
     decimals: number, description: string, initAnswer: bigint, intiUpdateAt: number, signer: ethers.Signer
 ) => {
     // デプロイ実行
-    const factory = createTrialOracleV1Factory().connect(signer);
+    const factory = new ethers.ContractFactory<any[], TrialOracleV1>(TrialOracleAbi.abi, TrialOracleBytecode.bytecode).connect(signer);
     const contract = await factory.deploy(decimals, description, initAnswer, intiUpdateAt);
     return await contract.waitForDeployment();
 };
-
-const createTrialOracleV1Factory = () => {
-    // const abi = ITrialOracleV1Data.abi;
-    const abi = JSON.parse(JSON.stringify(ITrialOracleV1Data.abi)) as typeof ITrialOracleV1Data.abi;
-    (abi as any[]).push({
-        "inputs": [
-            {
-                "internalType": "uint8",
-                "name": "decimals_",
-                "type": "uint8"
-            },
-            {
-                "internalType": "string",
-                "name": "description_",
-                "type": "string"
-            },
-            {
-                "internalType": "int256",
-                "name": "answer",
-                "type": "int256"
-            },
-            {
-                "internalType": "uint256",
-                "name": "updatedAt",
-                "type": "uint256"
-            }
-        ],
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-    });
-    const bytecode = TrialOracleV1Data.bytecode;
-
-    return new ethers.ContractFactory<any[], ITrialOracleV1>(abi, bytecode);
-}
